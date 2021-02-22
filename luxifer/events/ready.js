84f81c0440a00611da95ab8d2d7f95b802e5f65a
yvanermino <76566808+yvanermino@users.noreply.github.com>
2021-02-22 11:52:49 +0800

    Delete luxifer directory

--- luxifer/events/ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (client) => {
-  console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity(`${client.guilds.cache.size} SERVERS | %help for list of commands`, {
-    type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
-  });
-};
